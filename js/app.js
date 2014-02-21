d0954b8ba5a68f377a9dd85de15a90108a75d348
okendoken <philip.daineka@gmail.com>
2014-02-22 00:17:11 +0300

    pjax

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ $(function(){
         this.$sidebar.on('mouseout', $.proxy(this.collapseNavigation, this));
 
         this.checkNavigationState();
+
+        $(document).pjax('#sidebar a', '.content', {fragment: '.content'});
+        $(document).on('pjax:end', $.proxy(this.changeActiveNavigationItem, this))
     };
 
     SingAppView.prototype.checkNavigationState = function(){
@@ -25,6 +28,14 @@ $(function(){
 
     SingAppView.prototype.expandNavigation = function(){
         $('body').removeClass('nav-collapsed');
+    };
+
+    SingAppView.prototype.changeActiveNavigationItem = function(){
+        this.$sidebar.find('li.active').removeClass('active');
+
+        //credit: http://stackoverflow.com/a/8497143/1298418
+        var pageName = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+        this.$sidebar.find('a[href*="' + pageName + '"]').closest('li').addClass('active');
     };
 
     window.SingApp = new SingAppView();
