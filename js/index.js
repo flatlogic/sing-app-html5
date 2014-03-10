c333d75b35c512422f1dda8acc7af88530bd184e
okendoken <philip.daineka@gmail.com>
2014-03-10 16:57:00 +0300

    chart changes

--- js/index.js
+++ js/index.js
@@ -25,21 +25,105 @@ function initPointSparkline($el, data){
         spotRadius: 4.1,
         spotColor: Sing._white,
         valueSpots: {'0:': Sing._white},
-        highlightSpotColor: Sing._stateDanger,
-        highlightLineColor: Sing._stateDanger,
-        minSpotColor: Sing._stateDanger,
-        maxSpotColor: Sing._stateSuccess,
+        highlightSpotColor: Sing._grayLighter,
+        highlightLineColor: Sing._grayLighter,
+        minSpotColor: Sing._white,
+        maxSpotColor: Sing._white,
         tooltipFormat: new $.SPFormatClass(''),
         chartRangeMin: _(data).min() - 1
     });
 }
 
 function initSimpleChart(){
-    initPointSparkline($("#chart-simple"), [4,6,5,7,5,6]);
+    initPointSparkline($("#chart-simple"), [4,6,5,7,5]);
+}
+
+function initChangesChart(){
+    var chartHeight = 100;
+    var seriesData = [ [], [], [], [], [] ];
+    var random = new Rickshaw.Fixtures.RandomData(10000);
+
+    for (var i = 0; i < 32; i++) {
+        random.addData(seriesData);
+    }
+
+    var graph = new Rickshaw.Graph( {
+        element: document.getElementById("chart-changes"),
+        renderer: 'multi',
+        height: chartHeight,
+        series: [{
+            name: 'pop',
+            data: seriesData.shift().map(function(d) { return { x: d.x, y: d.y } }),
+            color: Sing.lighten(Sing._brandSuccess, .09),
+            renderer: 'bar'
+        }, {
+            name: 'humidity',
+            data: seriesData.shift().map(function(d) { return { x: d.x, y: d.y * (Math.random()*0.1 + 1.1) } }),
+            renderer: 'line',
+            color: Sing._white
+        }]
+    } );
+
+    function onResize(){
+        graph.configure({
+            width: $('#chart-changes').width(),
+            gapSize: 0.5,
+            min: 'auto',
+            strokeWidth: 3
+        });
+        graph.render();
+    }
+
+    SingApp.onResize(onResize);
+    onResize();
+
+    var detail = new Rickshaw.Graph.HoverDetail({
+        graph: graph
+    });
+
+    var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
+        graph: graph
+    });
+
+    var yAxis = new Rickshaw.Graph.Axis.Y({
+        graph: graph,
+        ticksTreatment: 'hide',
+        pixelsPerTick: chartHeight
+    });
+
+    yAxis.render();
+
+}
+
+function initChangesYearChart(){
+    var $el = $('#chart-changes-year'),
+        data = [3,6,2,4,5,8,6,8],
+        dataMax = _(data).max(),
+        backgroundData = data.map(function(){return dataMax});
+
+    $el.sparkline(backgroundData,{
+        type: 'bar',
+        height: 26,
+        barColor: Sing._grayLighter,
+        barWidth: 7,
+        barSpacing: 5,
+        chartRangeMin: _(data).min(),
+        tooltipFormat: new $.SPFormatClass('')
+    });
+
+    $el.sparkline(data,{
+        composite: true,
+        type: 'bar',
+        barColor: Sing._brandSuccess,
+        barWidth: 7,
+        barSpacing: 5
+    });
 }
 
 function pjaxPageLoad(){
     initSimpleChart();
+    initChangesChart();
+    initChangesYearChart();
 }
 
 $(function(){
--- js/settings.js
+++ js/settings.js
@@ -3,8 +3,91 @@ $(function(){
         _white: '#fff',
         _black: '#000',
         _grayLight: '#999',
-        _stateSuccess: '#8bdb63',
-        _stateDanger: '#fb6d5d'
+        _grayLighter: '#eee',
+        _brandPrimary: '#027ac1',
+        _brandSuccess: '#4aaa43',
+        _brandWarning: '#ffc427',
+        _brandDanger: '#e31a38',
+
+        //credit http://stackoverflow.com/questions/1507931/generate-lighter-darker-color-in-css-using-javascript
+        changeColor: function(color, ratio, darker) {
+            var pad = function(num, totalChars) {
+                var pad = '0';
+                num = num + '';
+                while (num.length < totalChars) {
+                    num = pad + num;
+                }
+                return num;
+            };
+            // Trim trailing/leading whitespace
+            color = color.replace(/^\s*|\s*$/, '');
+
+            // Expand three-digit hex
+            color = color.replace(
+                /^#?([a-f0-9])([a-f0-9])([a-f0-9])$/i,
+                '#$1$1$2$2$3$3'
+            );
+
+            // Calculate ratio
+            var difference = Math.round(ratio * 256) * (darker ? -1 : 1),
+            // Determine if input is RGB(A)
+                rgb = color.match(new RegExp('^rgba?\\(\\s*' +
+                    '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+                    '\\s*,\\s*' +
+                    '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+                    '\\s*,\\s*' +
+                    '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+                    '(?:\\s*,\\s*' +
+                    '(0|1|0?\\.\\d+))?' +
+                    '\\s*\\)$'
+                    , 'i')),
+                alpha = !!rgb && rgb[4] != null ? rgb[4] : null,
+
+            // Convert hex to decimal
+                decimal = !!rgb? [rgb[1], rgb[2], rgb[3]] : color.replace(
+                    /^#?([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])/i,
+                    function() {
+                        return parseInt(arguments[1], 16) + ',' +
+                            parseInt(arguments[2], 16) + ',' +
+                            parseInt(arguments[3], 16);
+                    }
+                ).split(/,/),
+                returnValue;
+
+            // Return RGB(A)
+            return !!rgb ?
+                'rgb' + (alpha !== null ? 'a' : '') + '(' +
+                    Math[darker ? 'max' : 'min'](
+                        parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+                    ) + ', ' +
+                    Math[darker ? 'max' : 'min'](
+                        parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+                    ) + ', ' +
+                    Math[darker ? 'max' : 'min'](
+                        parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+                    ) +
+                    (alpha !== null ? ', ' + alpha : '') +
+                    ')' :
+                // Return hex
+                [
+                    '#',
+                    pad(Math[darker ? 'max' : 'min'](
+                        parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+                    ).toString(16), 2),
+                    pad(Math[darker ? 'max' : 'min'](
+                        parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+                    ).toString(16), 2),
+                    pad(Math[darker ? 'max' : 'min'](
+                        parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+                    ).toString(16), 2)
+                ].join('');
+        },
+        lighten: function(color, ratio) {
+            return this.changeColor(color, ratio, false);
+        },
+        darken: function(color, ratio) {
+            return this.changeColor(color, ratio, true);
+        }
     };
 
     var SingSettingsBundle = function(){
