e675f035566df4cdbeffaf7352205f31b7c182b0
okendoken <philip.daineka@gmail.com>
2014-05-17 00:09:31 +0300

    charts

--- js/index.js
+++ js/index.js
@@ -113,84 +113,6 @@ $(function(){
         $('body').append($(".mapTooltip").detach());
     }
 
-    /* Inspired by Lee Byron's test data generator. */
-    function _stream_layers(n, m, o) {
-        if (arguments.length < 3) o = 0;
-        function bump(a) {
-            var x = 1 / (.1 + Math.random()),
-                y = 2 * Math.random() - .5,
-                z = 10 / (.1 + Math.random());
-            for (var i = 0; i < m; i++) {
-                var w = (i / m - y) * z;
-                a[i] += x * Math.exp(-w * w);
-            }
-        }
-        return d3.range(n).map(function() {
-            var a = [], i;
-            for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-            for (i = 0; i < 5; i++) bump(a);
-            return a.map(function(d, i) {
-                return {x: i, y: Math.max(0, d)};
-            });
-        });
-    }
-
-    function testData(stream_names, pointsCount) {
-        var now = new Date().getTime(),
-            day = 1000 * 60 * 60 * 24, //milliseconds
-            daysAgoCount = 60,
-            daysAgo = daysAgoCount * day,
-            daysAgoDate = now - daysAgo,
-            pointsCount = pointsCount || 45, //less for better performance
-            daysPerPoint = daysAgoCount / pointsCount;
-        return _stream_layers(stream_names.length, pointsCount, .1).map(function(data, i) {
-            return {
-                key: stream_names[i],
-                values: data.map(function(d,j){
-                    return {
-                        x: daysAgoDate + d.x * day * daysPerPoint,
-                        y: Math.floor(d.y * 100) //just a coefficient,
-                    }
-                })
-            };
-        });
-    }
-
-    function initBigChart(){
-
-        nv.addGraph(function() {
-            var chart = nv.models.lineChart()
-                .useInteractiveGuideline(true)
-                .showXAxis(false)
-                .showYAxis(true)
-                .margin({left: 28, bottom: 0, right: 0})
-                .color([Sing.colors['brand-danger'], Sing.colors['brand-warning'], Sing.colors['brand-success']]);
-
-            chart.xAxis
-                .showMaxMin(false)
-                .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)) });
-
-            chart.yAxis
-                .showMaxMin(false)
-                .ticks(0)
-                .tickFormat(d3.format(',f'));
-
-            d3.select('#chart svg')
-                .datum(testData(['Search', 'Referral', 'Direct'], 150).map(function(el, i){
-                    (i == 0) && (el.area = true);
-                    return el;
-                }))
-                .transition().duration(500)
-                .call(chart)
-            ;
-
-
-            SingApp.onResize(chart.update);
-
-            return chart;
-        });
-    }
-
     function initCalendar(){
 
         var monthNames = ["January", "February", "March", "April", "May", "June",  "July", "August", "September", "October", "November", "December"];
@@ -256,7 +178,6 @@ $(function(){
 
     function pjaxPageLoad(){
         initMap();
-//        initBigChart();
         initCalendar();
     }
 
