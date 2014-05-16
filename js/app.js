afcbcfc424e6aeb65bea4740cee35d30cc52ea06
okendoken <philip.daineka@gmail.com>
2014-05-16 16:07:33 +0300

    remove wrapping col- when widget closed fix

--- js/app.js
+++ js/app.js
@@ -614,7 +614,7 @@ function initAppFunctions(){
             var $colWrap = $(e.target).closest('.content > .row > [class*="col-"]:not(.widget-container)');
 
             // remove colWrap only if there are no more widgets inside
-            if (!$colWrap.find('> .widget').length){
+            if (!$colWrap.find('> .widget').not(e.target).length){
                 $colWrap.remove();
             }
         });
