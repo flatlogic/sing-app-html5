b8d637212832a232168bedeaa5fe60c875a90759
Artem <shoudaos92@gmail.com>
2016-01-05 16:38:43 +0300

    gulp styles for ajax version and bootstrap 3

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+var rename = require ('gulp-rename');
+
+gulp.task('styles', function () {
+
+    gulp.src('./sass/application.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+
+    gulp.src('./sass/application.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./css'));
+});
