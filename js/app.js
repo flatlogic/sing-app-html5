3602b010d5280cab134a3291ae701fad1c927ea3
Artem <shoudaos92@gmail.com>
2016-01-28 18:30:22 +0300

    fix Tooltip reposition

--- js/app.js
+++ js/app.js
@@ -617,6 +617,43 @@ function initAppPlugins(){
 
         $('.js-progress-animate').animateProgressBar();
     }(jQuery);
+
+    /* ========================================================================
+     * Reposition Tooltip
+     * ========================================================================
+     */
+    !function($){
+        $.fn.onPositionChanged = function (trigger, millis) {
+            if (millis == null) millis = 100;
+            var o = $(this[0]); // our jquery object
+            if (o.length < 1) return o;
+
+            var lastPos = null;
+            var lastOff = null;
+            setInterval(function () {
+                if (o == null || o.length < 1) return o; // abort if element is non existend eny more
+                if (lastPos == null) lastPos = o.position();
+                if (lastOff == null) lastOff = o.offset();
+                var newPos = o.position();
+                var newOff = o.offset();
+                if (lastPos.top != newPos.top || lastPos.left != newPos.left) {
+                    $(this).trigger('onPositionChanged', { lastPos: lastPos, newPos: newPos });
+                    if (typeof (trigger) == "function") trigger(lastPos, newPos);
+                    lastPos = o.position();
+                }
+                if (lastOff.top != newOff.top || lastOff.left != newOff.left) {
+                    $(this).trigger('onOffsetChanged', { lastOff: lastOff, newOff: newOff});
+                    if (typeof (trigger) == "function") trigger(lastOff, newOff);
+                    lastOff= o.offset();
+                }
+            }, millis);
+
+            return o;
+        };
+
+        $('#nav-state-toggle').onPositionChanged(function(){Tether.position();},0);
+        $('#nav-collapse-toggle').onPositionChanged(function(){Tether.position();},0);
+    }(jQuery);
 }
 
 /**
