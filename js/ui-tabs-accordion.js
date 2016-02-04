95f4746a55629ef6bd76363254d4845d668b0823
Artem <shoudaos92@gmail.com>
2016-02-04 16:38:40 +0300

    fix adding active class to dropdown in nav-tabs in ui-tabs-accordion

--- js/ui-tabs-accordion.js
+++ js/ui-tabs-accordion.js
@@ -1,24 +1,13 @@
 $(function () {
     function pageLoad() {
         removeActiveClass();
-        setActiveClass();
     }
 
     function removeActiveClass() {
-        var li = $('.dropdown').parent('.nav-tabs').find('.nav-item');
-        li.on('click', function () {
-            var a = $('.nav-tabs').find('.dropdown-menu').find('a.active');
-            if(a.length) {
-                a.removeClass('active');
+        $('.nav-tabs').on('shown.bs.tab', 'a', function (e) {
+            if (e.relatedTarget) {
+                $(e.relatedTarget).removeClass('active');
             }
-        });
-    }
-
-    function setActiveClass() {
-        var li = $('.nav-tabs').find('.dropdown-menu').find('li');
-        li.on('click', function(event) {
-            var a = $(event.currentTarget).parent('.dropdown-menu').parent().children('a');
-            a.addClass('active');
         });
     }
 
