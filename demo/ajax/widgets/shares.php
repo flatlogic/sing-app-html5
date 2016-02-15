8ecc0933bef20de68ca50efd54fb78595fa370a1
Artem <shoudaos92@gmail.com>
2016-02-15 14:36:06 +0300

    fix bugs

--- demo/ajax/widgets/shares.php
+++ demo/ajax/widgets/shares.php
@@ -5,7 +5,7 @@
             <img class="img-circle" src="demo/img/people/a6.jpg" alt="...">
         </span>
         <i class="fa fa-circle pull-xs-right text-success mt-sm"></i>
-        <h6 class="no-margin fs-sm"><strong>Jenny Wilington</strong></h6>
+        <h6 class="no-margin"><strong>Jenny Wilington</strong></h6>
         <small>just now</small>
     </a>
     <a href="#" class="list-group-item">
@@ -13,7 +13,7 @@
             <img class="img-circle" src="demo/img/people/a1.jpg" alt="...">
         </span>
         <i class="fa fa-circle pull-xs-right text-danger mt-sm"></i>
-        <h6 class="no-margin fs-sm">Maikel Basso</h6>
+        <h6 class="no-margin">Maikel Basso</h6>
         <small class="text-muted">about 2 mins ago</small>
     </a>
     <a href="#" class="list-group-item">
@@ -21,7 +21,7 @@
             <img class="img-circle" src="demo/img/people/a2.jpg" alt="...">
         </span>
         <i class="fa fa-circle pull-xs-right text-info mt-sm"></i>
-        <h6 class="no-margin fs-sm">Ianus Arendse</h6>
+        <h6 class="no-margin">Ianus Arendse</h6>
         <small class="text-muted">about 42 mins ago</small>
     </a>
     <a href="#" class="list-group-item">
@@ -29,7 +29,7 @@
             <img class="img-circle" src="demo/img/people/a3.jpg" alt="...">
         </span>
         <i class="fa fa-circle pull-xs-right text-success mt-sm"></i>
-        <h6 class="no-margin fs-sm">Valdemar Landau</h6>
+        <h6 class="no-margin">Valdemar Landau</h6>
         <small class="text-muted">one hour ago</small>
     </a>
     <a href="#" class="list-group-item mb-n-md">
@@ -37,7 +37,7 @@
             <img class="img-circle" src="demo/img/people/a4.jpg" alt="...">
         </span>
         <i class="fa fa-circle pull-xs-right text-warning mt-sm"></i>
-        <h6 class="no-margin fs-sm">Rick Teagan</h6>
+        <h6 class="no-margin">Rick Teagan</h6>
         <small class="text-muted">3 hours ago</small>
     </a>
 </div>
