b1a05287b7775320b1f8e373b637bda28cafb962
shoudaos <shoudaos92@gmail.com>
2018-02-27 16:46:15 +0300

    Pr/html bs4/product grid adaptivity (#40)
    * fix(product-grid): add mobile filter dropdowns
    
    * fix(product-grid): add mobile filter functionality

--- js/product-grid.js
+++ js/product-grid.js
@@ -1,5 +1,92 @@
 $(function() {
+  var filters = {
+    type: {items: ['Shoes', 'Boots', 'Trainers'], checked: ''},
+    brands: {items: ['All', 'Nike', 'Adidas'], checked: ''},
+    size: {items: ['All', '7', '8', '9', '10', '11', '12'], checked: ''},
+    color: {items: ['All', 'White', 'Black'], checked: ''},
+    range: {items: ['All', '0 - 49', '50 - 149', '150 - 499', '500+'], checked: ''}
+  };
+
+  function showFiltersModal() {
+    $('.product-grid__filter-modal').addClass('show');
+    $('.product-grid__main').addClass('hide');
+  }
+
+  function showFiltersList(list) {
+    $(list).addClass('show').siblings().removeClass('show');
+  }
+
+  function declareModalFilterTitle(title) {
+    $('.product-grid__filter-modal-title > h5').html(title);
+  }
+
+  function showButton(id) {
+    $('.product-grid__filter-modal-title #' + id)
+        .addClass('show')
+        .removeClass('hide')
+        .siblings()
+        .removeClass('show')
+        .addClass('hide');
+  }
+
+  function hideFiltersModal() {
+    $('.product-grid__filter-modal').removeClass('show');
+    $('.product-grid__main').removeClass('hide');
+  }
+
+  function generateSubFilterMenu(listObjectName) {
+    var listObject = filters[listObjectName];
+    var $list = '';
+    listObject.items.forEach(function (l, index) {
+      if (listObject.checked === l) {
+        $list += '<li class="option active"><span class="filter-check"><img src="./demo/img/check.svg" alt="check" /></span><span class="name">'+ l +'</span></li>';
+      } else {
+        $list += '<li class="option"><span class="filter-check"><img src="./demo/img/check.svg" alt="check" /></span><span class="name">'+ l +'</span></li>';
+      }
+
+    });
+
+    $('#type-subfilters-list').empty().append($list).find('li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        filters[listObjectName].checked = $(this).find('.name').html();
+        console.log(filters[listObjectName].checked);
+    });
+  }
+
+
+  function initFilter() {
+    $('#sort-filters').on('click', function () {
+      showFiltersModal();
+      showFiltersList('#sort-filters-list');
+      declareModalFilterTitle('Sort');
+      showButton('close_modal');
+    });
+    $('#other-filters').on('click', function () {
+      showFiltersModal();
+      showFiltersList('#type-filters-list');
+      declareModalFilterTitle('Filters');
+      showButton('close_modal');
+    });
+    $('#back_modal').on('click', function() {
+      showFiltersList('#type-filters-list');
+      declareModalFilterTitle('Filters');
+      showButton('close_modal');
+    });
+    $('#close_modal').on('click', function() {
+        hideFiltersModal();
+    });
+    $('ul#type-filters-list > li').on('click', function () {
+      var list = $(this).html();
+      showFiltersList('#type-subfilters-list');
+      declareModalFilterTitle(list);
+      showButton('back_modal');
+      generateSubFilterMenu(list.toLowerCase());
+    })
+  }
+
   function pageLoad() {
+    initFilter();
+
     $(".star").on("click", function() {
       var starred = $(this).hasClass("star--fill");
 
