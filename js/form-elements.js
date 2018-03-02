8eb874f5819f9b4994c70ef4796597884921005b
shoudaos <shoudaos92@gmail.com>
2018-03-02 17:01:24 +0300

    fix(layout): fix styles, fix form-elements page

--- js/form-elements.js
+++ js/form-elements.js
@@ -3,11 +3,11 @@ $(function(){
     $('.widget').widgster();
     var bs3Wysihtml5Templates = {
         "emphasis": function(locale, options) {
-            var size = (options && options.size) ? ' btn-'+options.size : '';
+            var size = (options && options.size) ? 'btn-'+options.size : '';
             return "<li>" +
                 "<div class='btn-group'>" +
-                "<a class='btn btn-" + size + " btn-secondary' data-wysihtml5-command='bold' title='CTRL+B' tabindex='-1'><i class='fa fa-bold'></i></a>" +
-                "<a class='btn btn-" + size + " btn-secondary' data-wysihtml5-command='italic' title='CTRL+I' tabindex='-1'><i class='fa fa-italic'></i></a>" +
+                "<a class='btn " + size + " btn-default' data-wysihtml5-command='bold' title='CTRL+B' tabindex='-1'><i class='fa fa-bold'></i></a>" +
+                "<a class='btn " + size + " btn-default' data-wysihtml5-command='italic' title='CTRL+I' tabindex='-1'><i class='fa fa-italic'></i></a>" +
                 "</div>" +
                 "</li>";
         },
@@ -33,14 +33,14 @@ $(function(){
                 "</div>" +
                 "</div>" +
                 "</div>" +
-                "<a class='btn btn-" + size + " btn-secondary' data-wysihtml5-command='createLink' title='" + locale.link.insert + "' tabindex='-1'><i class='fa fa-share'></i></a>" +
+                "<a class='btn " + size + " btn-default' data-wysihtml5-command='createLink' title='" + locale.link.insert + "' tabindex='-1'><i class='fa fa-share'></i></a>" +
                 "</li>";
         },
         "html": function(locale, options) {
             var size = (options && options.size) ? ' btn-'+options.size : '';
             return "<li>" +
                 "<div class='btn-group'>" +
-                "<a class='btn btn-" + size + " btn-secondary' data-wysihtml5-action='change_view' title='" + locale.html.edit + "' tabindex='-1'><i class='fa fa-pencil'></i></a>" +
+                "<a class='btn " + size + " btn-default' data-wysihtml5-action='change_view' title='" + locale.html.edit + "' tabindex='-1'><i class='fa fa-pencil'></i></a>" +
                 "</div>" +
                 "</li>";
         }
