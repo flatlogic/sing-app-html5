c7560c7285dfa3012010ca9582c0f9286196db80
shoudaos <shoudaos92@gmail.com>
2018-03-16 18:41:36 +0300

    feat(gulp): add build script

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var sass = require('gulp-sass');
 
 var rename = require ('gulp-rename');
 
+var clean = require('gulp-clean');
+
 gulp.task('styles', function () {
 
     gulp.src('./sass/application.scss')
@@ -26,3 +28,25 @@ gulp.task('styles', function () {
 gulp.task('watch', function () {
     gulp.watch('./sass/*.scss', ['styles']);
 });
+
+gulp.task('clean', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('copy', ['clean'], function () {
+    gulp.src('./*.html')
+        .pipe(gulp.dest('./dist'));
+    gulp.src('./css/**/*')
+        .pipe(gulp.dest('./dist/css'));
+    gulp.src('./demo/**/*')
+        .pipe(gulp.dest('./dist/demo'));
+    gulp.src('./js/**/*')
+        .pipe(gulp.dest('./dist/js'));
+    gulp.src('./img/**/*')
+        .pipe(gulp.dest('./dist/img'));
+    gulp.src('./vendor/**/*')
+        .pipe(gulp.dest('./dist/vendor'));
+});
+
+gulp.task('build', ['styles', 'copy']);
