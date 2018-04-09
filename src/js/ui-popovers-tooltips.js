cdc7eaed0da374cfa3e13f60adf8877d3343d673
Ilya Kulgavy <i.kulgavy@gmail.com>
2018-04-09 18:22:05 +0300

    fixes: tables, buttons & popovers

--- src/js/ui-popovers-tooltips.js
+++ src/js/ui-popovers-tooltips.js
@@ -3,7 +3,7 @@ $(function () {
   function pageLoad() {
     $('.widget').widgster();
     $('[data-toggle=tooltip]').tooltip();
-    $('[data-toggle=popover]').popover();
+    $('[data-toggle=popover]').popover({trigger: 'focus'});
   }
 
   pageLoad();
