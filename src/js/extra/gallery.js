0d873f09439a5c4b16f5159e1d5d88dbd9451ac1
Ilya Kulgavy <i.kulgavy@gmail.com>
2018-10-05 15:16:55 +0300

    packages update

--- src/js/extra/gallery.js
+++ src/js/extra/gallery.js
@@ -6,27 +6,16 @@ $(function(){
             var $grid = $('#grid'),
                 $filterOptions = $('.js-filter-options > .filter'),
                 $sizer = $grid.find('.js-shuffle-sizer'),
+                shuffleInstance = new Shuffle($grid, {
+                    itemSelector: '.gallery-item',
+                    sizer: $sizer // could also be a selector: '.my-sizer-element'
+                });
 
-                init = function() {
-                    setTimeout(function() {
-                        $grid.shuffle( 'shuffle', 'all' );
-                    }, 0);
-
+                var init = function() {
                     setupFilters();
                     setupSorting();
-                    setupSearching();
 
-                    SingApp.onResize(function(){
-                        $grid.shuffle('update');
-                    });
-
-                    // instantiate the plugin
-                    $grid.shuffle({
-                        itemSelector: '.gallery-item',
-                        sizer: $sizer
-                    });
-
-
+                    shuffleInstance.filter('all');
                 },
 
             // Set up button clicks
@@ -38,11 +27,9 @@ $(function(){
 
                         // Hide current label, show current label in title
                         $('.js-filter-options .active').removeClass('active');
-
                         $this.addClass('active');
 
-                        // Filter elements
-                        $grid.shuffle( 'shuffle', group );
+                        shuffleInstance.filter(group);
                     });
 
                     $btns = null;
@@ -55,31 +42,14 @@ $(function(){
                             opts = {
                             reverse: order === 'desc',
                             by: function($el) {
-                                return $el.data('title').toLowerCase();
+                                return $el.getAttribute('data-title').toLowerCase();
                             }
 
                         };
                         $('.js-sort-options .active').removeClass('active');
 
                         $(this).addClass('active');
-                        $grid.shuffle('sort', opts);
-                    });
-                },
-
-                setupSearching = function() {
-                    // Advanced filtering
-                    $('.js-shuffle-search').on('keyup change', function() {
-                        var val = this.value.toLowerCase();
-                        $grid.shuffle('shuffle', function($el, shuffle) {
-
-                            // Only search elements in the current group
-                            if (shuffle.group !== 'all' && $.inArray(shuffle.group, $el.data('groups')) === -1) {
-                                return false;
-                            }
-
-                            var text = $.trim( $el.find('.picture-item__title').text() ).toLowerCase();
-                            return text.indexOf(val) !== -1;
-                        });
+                        shuffleInstance.sort(opts);
                     });
                 };
 
